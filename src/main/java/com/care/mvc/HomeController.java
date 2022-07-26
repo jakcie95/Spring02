7b75c70967241bca11891943d15ef3e07cd83c65
jungho <jakcie95@n.n>
2022-07-26 21:43:10 +0900

    스프링 2일차 mvc_02

--- src/main/java/com/care/mvc/HomeController.java
+++ src/main/java/com/care/mvc/HomeController.java
@@ -1,15 +1,20 @@
 package com.care.mvc;
 
+import java.lang.ProcessBuilder.Redirect;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * Handles requests for the application home page.
@@ -35,5 +40,38 @@ public class HomeController {
 		
 		return "home";
 	}
-	
+	@GetMapping("re/index")
+	public void index() {}
+	@RequestMapping("re/result")
+	public String result(HttpServletRequest request,
+									Model model,
+									RedirectAttributes rs) {
+		String id = request.getParameter("id");
+		if(id.equals("abc")) {
+			model.addAttribute("result", "로그인 성공");
+			rs.addFlashAttribute("re","re로그인성공==");
+			return "redirect:rsOK";
+		}
+		return "redirect:rsNO";
+	}
+	@GetMapping("re/rsOK")
+	public String ok(String result, Model model) {
+		model.addAttribute("result", result);
+		System.out.println("result : "+result);
+		return "re/rsOK";
+	}
+	@GetMapping("re/rsNO")
+	public String no() {
+		return "re/rsNO";
+	}
 }
+
+
+
+
+
+
+
+
+
+
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -10,5 +10,17 @@
 </h1>
 
 <P>  The time on the server is ${serverTime}. </P>
+<hr>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<c:set var="contextPath" value="${pageContext.request.contextPath }"/>
+
+path : <%=request.getContextPath() %><br>
+
+path : ${contextPath }<br>
+
+
+<!-- 편한 방법 사용 -->
+<img src="${contextPath }/resources/images/img.jpg" width="100" height="100">
+<img src='<c:url value="/img/img.jpg" />' width="100" height="100">
 </body>
 </html>
